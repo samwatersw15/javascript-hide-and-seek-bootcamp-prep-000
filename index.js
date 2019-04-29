8340c40db624be1311e0c4b4c6518323a399600c
Sam Waters <samwatersw15@gmail.com>
2019-04-29 19:50:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
+function getFirstSelector(selector) {
+  let a = document.querySelector(selector)
+  return a 
+}
 
+function nestedTarget() {
+  let a = document
+  .querySelector('#nested')
+  .querySelector('.target')
+  return a
+}
+
+function increaseRankBy(n) {
+  let a = document
+  .querySelectorAll('ul.ranked-list li')
+  
+  for (let i = 0; i<a.length; i++) {
+    a[i].innerHTML = parseInt(a[i].innerHTML) + n
+  }
+  
+}
+
+function deepestChild() {
+  let a = document
+  .querySelectorAll('div#grand-node')
+  for (let i = 0; i<a.length; i++) {
+    let b = a[i].querySelector('div')
+    if (b.hasChildNodes()) {
+      return b
+    }
+  }
+  
+  }
