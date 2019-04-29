da207c46bcda79e0f9f035807ad7e44996682648
Sam Waters <samwatersw15@gmail.com>
2019-04-29 20:10:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
+function getFirstSelector(selector) {
+  let a = document.querySelector(selector)
+  return a 
+}
 
+function nestedTarget() {
+  let a = document
+  .querySelector('#nested')
+  .querySelector('.target')
+  return a
+}
+
+function increaseRankBy(n) {
+  let a = document
+  .querySelectorAll('ul.ranked-list li')
+  
+  for (let i = 0; i<a.length; i++) {
+    a[i].innerHTML = parseInt(a[i].innerHTML) + n
+  }
+  
+}
+
+function deepestChild() {
+  let a = document
+  .querySelector('div#grand-node')
+  .querySelectorAll('div')
+  for (let i = 0; i<a.length; i++) {
+   if (!a[i].querySelector('div')) {
+     return a[i]
+   }
+  }
+  }
